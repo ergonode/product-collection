86531ac9d77d2aa3d8ccbe632a1ef6adf031b2c5
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2020-06-04 13:36:45 +0200

    feature(products) add adding child by sku endpoint (#591)

--- src/Application/Form/ProductCollectionElementFromSkusForm.php
+++ src/Application/Form/ProductCollectionElementFromSkusForm.php
@@ -9,17 +9,15 @@ declare(strict_types = 1);
 
 namespace Ergonode\ProductCollection\Application\Form;
 
-use Ergonode\ProductCollection\Application\Model\ProductCollectionElementMultipleCreateFormModel;
-use Ergonode\Segment\Application\Form\Type\SegmentType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Ergonode\ProductCollection\Application\Model\ProductCollectionElementFromSkusFormModel;
 
 /**
  */
-class ProductCollectionElementMultipleCreateForm extends AbstractType
+class ProductCollectionElementFromSkusForm extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
@@ -28,15 +26,6 @@ class ProductCollectionElementMultipleCreateForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add(
-                'segments',
-                CollectionType::class,
-                [
-                    'allow_add' => true,
-                    'allow_delete' => true,
-                    'entry_type' => SegmentType::class,
-                ]
-            )
             ->add(
                 'skus',
                 TextareaType::class
@@ -49,8 +38,8 @@ class ProductCollectionElementMultipleCreateForm extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => ProductCollectionElementMultipleCreateFormModel::class,
-            'translation_domain' => 'element',
+            'data_class' => ProductCollectionElementFromSkusFormModel::class,
+            'translation_domain' => 'product-collection',
         ]);
     }
 
